274a9a3674d3c82888be67a4aff2d66b86da0bc4
Maximiliano Diaz Clemente <233306@ids.upchiapas.edu.mx>
2026-02-04 19:05:40 -0600

    Cors implementado

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
-
-from app.routes import user_routes
-from app.shared.config.database import engine, Base, SessionLocal
-
+from fastapi.middleware.cors import CORSMiddleware
+from app.shared.config.database import engine, Base
 from app.routes.user_routes import user_router
 from app.routes.recipie_routes import recipie_router
 from app.routes.list_routes import list_routes
@@ -12,6 +10,14 @@ app = FastAPI()
 app.include_router(user_router, prefix="/api", tags=["users"])
 app.include_router(recipie_router, prefix="/api", tags=["recipes"])
 app.include_router(list_routes, prefix="/api", tags=["lists"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 Base.metadata.create_all(bind=engine)
